fff77dc5cd8f3f8cd5c22f706f41b48f3f0c63ce
lisaw97 <lisawen0614@gmail.com>
2020-01-15 16:07:19 -0800

    Create App function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  // todos: state
+  // setTodos: update state
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about React',
+      isCompleted: false
+    },
+    {
+      text: 'Meet friend for dinner',
+      isCompleted: false
+    },
+    {
+      text: 'Build todo app with React Hooks',
+      isCompleted: false
+    }
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => {
+          <Todo key={index} index={index} todo={todo} />
+        })}
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
